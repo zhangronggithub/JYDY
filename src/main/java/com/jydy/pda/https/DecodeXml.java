ce4d7c5732722fe3c130d031a93352a7c7649444
23923 <3512301488@qq.com>
2017-05-08 13:41:03 +0800

    dataset数据解析方法

--- src/main/java/com/jydy/pda/https/DecodeXml.java
+++ src/main/java/com/jydy/pda/https/DecodeXml.java
@@ -19,46 +19,47 @@ import static android.view.View.Y;
 
 public class DecodeXml {
 
-    String  TAG = getClass().getSimpleName();
+    String TAG = getClass().getSimpleName();
 
     /**
      * 用来解析没有重复标签的xml字符串
+     *
      * @param content 待解析的xml字符串
-     * @param key 标签
+     * @param key     标签
      * @return 标签的内容
      * @throws Exception
      */
-    public static String decodeXml(String content,String key) throws Exception {
+    public static String decodeXml(String content, String key) throws Exception {
 
         // 由android.util.Xml创建一个XmlPullParser实例
         XmlPullParser parser = Xml.newPullParser();
         // 设置输入流 并指明编码方式
         parser.setInput(new StringReader(content));
         // 产生第一个事件
-        int eventType = parser.getEventType();  
-          
-        while (eventType != XmlPullParser.END_DOCUMENT){  
-             switch (eventType) {
-                 // 判断当前事件是否为文档开始事件
-                         case XmlPullParser.START_DOCUMENT:  
-                             break;
-                 // 判断当前事件是否为标签元素开始事件
-                         case XmlPullParser.START_TAG:  
-                             if (parser.getName().equals(key)) { // 判断开始标签元素是否是key
-                            	 eventType = parser.next();//让解析器指向key属性的值
-                            	 return parser.getText();
-                             }
-                             break;
+        int eventType = parser.getEventType();
 
-                 // 判断当前事件是否为标签元素结束事件
-                         case XmlPullParser.END_TAG:  
-                             break;  
-                         }
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            switch (eventType) {
+                // 判断当前事件是否为文档开始事件
+                case XmlPullParser.START_DOCUMENT:
+                    break;
+                // 判断当前事件是否为标签元素开始事件
+                case XmlPullParser.START_TAG:
+                    if (parser.getName().equals(key)) { // 判断开始标签元素是否是key
+                        eventType = parser.next();//让解析器指向key属性的值
+                        return parser.getText();
+                    }
+                    break;
+
+                // 判断当前事件是否为标签元素结束事件
+                case XmlPullParser.END_TAG:
+                    break;
+            }
             // 进入下一个元素并触发相应事件
-                         eventType = parser.next();
-                     }
-        return null;
+            eventType = parser.next();
         }
+        return null;
+    }
 
     /**
      * 解析有循环标签的xml字符串
@@ -115,45 +116,45 @@ public class DecodeXml {
 //        return mList;
 //
 //    }
+
     /**
      * 待测试
      * 解析dataset数据
-     * @param str 待解析数据
+     *
+     * @param str  待解析数据
      * @param flag 分割的标识
      * @return
      */
 //    D/ContentValues: anyType{schema=anyType{element=anyType{complexType=anyType{choice=anyType{element=anyType{complexType=anyType{sequence=anyType{element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; element=anyType{}; }; }; }; }; }; }; };
-// diffgram=anyType{NewDataSet=anyType{TAB_ZYLX=anyType{ZYLX001=50001; ZYLX002=作业类型01; ZYLX003=Y; ZYLX004=anyType{}; }; }; }; }
+//    diffgram=anyType{NewDataSet=anyType{TAB_ZYLX=anyType{ZYLX001=50001; ZYLX002=作业类型01; ZYLX003=Y; ZYLX004=anyType{}; }; }; }; }
 //    TAB_ZYLX=anyType{ZYLX001=50001; ZYLX002=作业类型01; ZYLX003=Y; ZYLX004=anyType{};
 //    ZYLX001=50001; ZYLX002=作业类型01; ZYLX003=Y; ZYLX004=anyType{};
-    //4
-    public static ArrayList<Map<String, Object>> decodeDataset(String str, String flag){
+//    4
+    public static ArrayList<Map<String, Object>> decodeDataset(String str, String flag) {
 
         Map<String, Object> mapOne = new HashMap<String, Object>();
 
-        Pattern pattern=Pattern.compile(flag+"=anyType\\{[^}]*\\};");
+        Pattern pattern = Pattern.compile(flag + "=anyType\\{[^}]*\\};");
 
         Matcher m = pattern.matcher(str);//import java.util.regex.Matcher;
-        ArrayList<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
-        while (m.find())
-        {
-            Logs.d("DecodeDataset",m.group());
-            String tmp=m.group().replace(flag+"=anyType{","");
-            Logs.d("DecodeDataset",tmp);
-            String[] Strlen=tmp.split(";");
+        ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        while (m.find()) {
+            Logs.d("DecodeDataset", m.group());
+            String tmp = m.group().replace(flag + "=anyType{", "");
+            Logs.d("DecodeDataset", tmp);
+            String[] Strlen = tmp.split(";");
 
-            if(Strlen.length>0)
-            {
-                Logs.d("DecodeDataset",Strlen.length);
+            if (Strlen.length > 0) {
+                Logs.d("DecodeDataset", Strlen.length);
                 mapOne = new HashMap<String, Object>(); //只要新建1个new map，就能预防值被覆盖的情况
                 for (int i = 0; i < Strlen.length; i++) {
-                    if (Strlen[i].split("=").length>1) {
-                        String key=Strlen[i].split("=")[0].trim();
-                        String value=Strlen[i].split("=")[1].trim();
+                    if (Strlen[i].split("=").length > 1) {
+                        String key = Strlen[i].split("=")[0].trim();
+                        String value = Strlen[i].split("=")[1].trim();
                         Logs.d("DecodeDataset", i);
                         Logs.d("DecodeDataset", key);
                         Logs.d("DecodeDataset", value);
-                        mapOne.put(key,value);
+                        mapOne.put(key, value);
                     }
                 }
 
